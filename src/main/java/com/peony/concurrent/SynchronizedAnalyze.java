7e7ee96eada6b75f8b271a6297f1bfb0b9557ed3
BlackHe <1124681011@qq.com>
2018-12-12 10:14:33 +0800

    分析其锁原理，线程处理机制

--- src/main/java/com/peony/concurrent/SynchronizedAnalyze.java
+++ src/main/java/com/peony/concurrent/SynchronizedAnalyze.java
@@ -5,4 +5,12 @@ package com.peony.concurrent;
  * 分析其锁原理，线程处理机制
  */
 public class SynchronizedAnalyze {
+
+    public synchronized int  sum(){
+        return 1;
+    }
+
+    public int count(){
+        return 2;
+    }
 }
