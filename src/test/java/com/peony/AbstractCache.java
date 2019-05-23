b43c3feb6f532bdaed0c0ac5e6c76b2a0748df95
BlackHe <1124681011@qq.com>
2019-05-23 15:26:01 +0800

    abstract method

--- src/test/java/com/peony/AbstractCache.java
+++ src/test/java/com/peony/AbstractCache.java
@@ -1,4 +1,20 @@
 package com.peony;
 
+/**
+ * abstract class
+ *
+ */
 public abstract class AbstractCache {
+
+    /**
+     * abstract method can't have method body
+     */
+    public abstract void edit();
+
+    /**
+     * usual method must be have method body
+     */
+    public void add(){
+
+    }
 }
---
+++ src/test/java/com/peony/Cache.java
@@ -0,0 +1,9 @@
+package com.peony;
+
+public class Cache extends AbstractCache {
+
+    @Override
+    public void edit() {
+
+    }
+}
