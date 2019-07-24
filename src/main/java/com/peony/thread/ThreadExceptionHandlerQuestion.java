f06106b55a0526490f11ffd5c1b7b707010c649a
BlackHe <1124681011@qq.com>
2019-07-24 13:43:33 +0800

    thread exception handler question implement by java 8 lambda express

--- src/main/java/com/peony/thread/ThreadExceptionHandlerQuestion.java
+++ src/main/java/com/peony/thread/ThreadExceptionHandlerQuestion.java
@@ -2,13 +2,23 @@ package com.peony.thread;
 
 public class ThreadExceptionHandlerQuestion {
     public static void main(String[] args) {
-        Thread.setDefaultUncaughtExceptionHandler( (thread,throwable) -> {
-            System.out.printf("thread name : [%s] , exception message : [%s]\n",thread.getName(),throwable.getMessage());
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            System.out.printf("thread name : [%s] , exception message : [%s]\n", thread.getName(), throwable.getMessage());
         });
 
         new Thread(() -> {
             System.out.printf("execute...\n");
             throw new RuntimeException("order id cant't be repeat");
-        },"thread-001").start();
+        }, "thread-001").start();
+
+        new Thread(ThreadExecute::action,"thread-002").start();
+    }
+
+    private static class ThreadExecute{
+
+        private static void action() {
+            System.out.printf("execute...\n");
+            throw new RuntimeException("order id cant't be repeat");
+        }
     }
 }
