e8c7ec2c9d0ba59c9f3ce676f6a529ae283fb4b7
BlackHe <1124681011@qq.com>
2019-07-26 15:30:05 +0800

    1、利用Executors提供的方法，获取线程池 2、静态导包应用 import static

---
+++ src/main/java/com/peony/thread/pool/ThreadPoolCreateByExecutorsQuestion.java
@@ -0,0 +1,29 @@
+package com.peony.thread.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+//import java.util.concurrent.Executors;
+
+// 静态导包 since 1.54
+import static  java.util.concurrent.Executors.*;
+
+/**
+ * 通过Executors获取线程池
+ */
+public class ThreadPoolCreateByExecutorsQuestion {
+
+    public static void main(String[] args) {
+        // 创建一个具有固定数量的线程池
+        ExecutorService threadPool1 = newFixedThreadPool(5);
+
+        // 创建一个最大整数值的线程池，没有corePoolSize,空闲60s销毁空闲线程
+        ExecutorService threadPool2 = newCachedThreadPool();
+
+        // 创建只有一个线程的线程池，永久存在，
+        ExecutorService threadPool3 = newSingleThreadExecutor();
+
+        // 创建一个 核心线程=5，maximum = Integer.MAX的线程池
+        ScheduledExecutorService threadPool4 = newScheduledThreadPool(5);
+    }
+}
