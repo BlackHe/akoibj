da2ca529d3de2ba5cda18f365ed16a79a9d800e9
BlackHe <1124681011@qq.com>
2019-07-26 11:04:56 +0800

    线程池测试

--- src/main/java/com/peony/thread/pool/ThreadPoolCreateQuestion.java
+++ src/main/java/com/peony/thread/pool/ThreadPoolCreateQuestion.java
@@ -1,30 +1,47 @@
 package com.peony.thread.pool;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class ThreadPoolCreateQuestion {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ExecutorService threadPool = getThreadPool();
         threadPool.execute(ThreadPoolCreateQuestion::printState);
         threadPool.shutdown();
     }
 
-    public static ExecutorService getThreadPool() {
+    public static ExecutorService getThreadPool() throws InterruptedException {
+        // create one task queue of capcity is thress, add three task to the queue;
+        BlockingQueue<Runnable> tasks = new LinkedBlockingQueue<Runnable>();
+        for (int i = 0; i < 1000; i++) {
+            tasks.add(() -> {
+                try {
+                    Thread.sleep(100);
+                }catch (InterruptedException e){
+                    System.out.println(e);
+                }
+                System.out.printf("task-[%d] was executed..by thread [%s]\n", 1, Thread.currentThread().getName());
+            });
+        }
+//        tasks.add(() -> {
+//            System.out.printf("task-[%d] was executed..by thread [%s]\n",2,Thread.currentThread().getName());
+//        });
+//        tasks.add(() -> {
+//            System.out.printf("task-[%d] was executed..by thread [%s]\n",3,Thread.currentThread().getName());
+//        });
+        // create one thread pool of core pool size is five, add task to the pool;
         ExecutorService threadPool = new ThreadPoolExecutor(
                 5,
                 10,
                 30,
                 TimeUnit.MINUTES,
-                new LinkedBlockingQueue<Runnable>());
+                tasks
+        );
         return threadPool;
     }
 
     private static void printState() {
-        System.err.printf("线程【%s】正在执行,当前的线程状态是【%s】", Thread.currentThread().getName(), Thread.currentThread().getState());
+//        System.err.printf("线程【%s】正在执行,当前的线程状态是【%s】", Thread.currentThread().getName(), Thread.currentThread().getState());
 
     }
 }
