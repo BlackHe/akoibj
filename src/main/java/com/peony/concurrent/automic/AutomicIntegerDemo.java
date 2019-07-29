f3be99228edb08a0e2288f06d62a50c6df6a2663
BlackHe <1124681011@qq.com>
2019-07-29 15:10:53 +0800

    原子类是不使用锁实现并发性的基础

--- src/main/java/com/peony/concurrent/automic/AutomicIntegerDemo.java
+++ src/main/java/com/peony/concurrent/automic/AutomicIntegerDemo.java
@@ -2,6 +2,9 @@ package com.peony.concurrent.automic;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * 原子类是不使用锁实现并发性的基础
+ */
 public class AutomicIntegerDemo {
 
     public static void main(String[] args) {
