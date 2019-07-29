e5f4008e27e4f0ea8e6e7b5fc62393dbf9094c84
BlackHe <1124681011@qq.com>
2019-07-29 15:18:14 +0800

    原子类是不使用锁实现并发性的基础

--- src/main/java/com/peony/concurrent/automic/AutomicIntegerDemo.java
+++ src/main/java/com/peony/concurrent/automic/AutomicIntegerDemo.java
@@ -7,22 +7,39 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class AutomicIntegerDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         CountService countService = new CountService();
-        new Thread(countService).start();
-        new Thread(countService).start();
-        new Thread(countService).start();
-        new Thread(countService).start();
-        new Thread(countService).start();
+        Thread thread1 = new Thread(countService);
+        thread1.start();
+        thread1.join();
+
+        Thread thread2 = new Thread(countService);
+        thread2.start();
+        thread2.join();
+
+        Thread thread3 = new Thread(countService);
+        thread3.start();
+        thread3.join();
+
+        Thread thread4 = new Thread(countService);
+        thread4.start();
+        thread4.join();
+
+        Thread thread5 = new Thread(countService);
+        thread5.start();
+        thread5.join();
+
+        System.out.println(CountService.count.get());
+
     }
 
 
     static class CountService implements Runnable {
-        private AtomicInteger count = new AtomicInteger(0);
+        static AtomicInteger count = new AtomicInteger(0);
 
         public void run() {
-            for (int i = 0; i < 10000; ++i) {
-                System.out.println(count.incrementAndGet());
+            for (int i = 0; i < 100; ++i) {
+                System.out.printf("线程【%s】正在操作自增,当前自增后的结果值是【%d】\n",Thread.currentThread().getName(),count.incrementAndGet());
             }
         }
     }
