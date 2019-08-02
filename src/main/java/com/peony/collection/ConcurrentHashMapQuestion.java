ff603d95834667f3d8be1d4160eabac14611b46a
BlackHe <1124681011@qq.com>
2019-08-02 13:23:34 +0800

    concurrentHashMap阐述

--- src/main/java/com/peony/collection/ConcurrentHashMapQuestion.java
+++ src/main/java/com/peony/collection/ConcurrentHashMapQuestion.java
@@ -10,8 +10,39 @@ public class ConcurrentHashMapQuestion {
     public static void main(String[] args) {
         ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>(16);
         // ConcurrentHashMap的key和value都不能为null,否则后抛出NPE
-        concurrentHashMap.put("name",null);
+//        concurrentHashMap.put("name",null);
         // 采用了分段锁的机制，锁的粒度缩小到Node节点级别
 
+        Hnode hnode = new Hnode("china",
+                new Hnode("gansu",
+                        new Hnode("linxia",
+                                new Hnode("beiyuan",
+                                        new Hnode("qianshi", null)))));
+        String print = hnode.print();
+        System.out.println(print);
+
+    }
+
+    static class Hnode {
+        private String key;
+        private Hnode next;
+
+        Hnode(String key, Hnode next) {
+            this.key = key;
+            this.next = next;
+        }
+
+        Hnode() {
+        }
+
+        public String print() {
+            Hnode hnode = this;
+            String sb = "[";
+            do {
+                sb = sb + "|" + hnode.key;
+            } while ((hnode = hnode.next) != null);
+            sb = sb + "]";
+            return sb;
+        }
     }
 }
