c639603fe029d4de5fca584e23187f70a2e9f435
BlackHe <1124681011@qq.com>
2019-08-12 20:37:17 +0800

    对象深拷贝和浅拷贝问题

--- src/main/java/com/peony/objclass/ObjectCopyQuestion.java
+++ src/main/java/com/peony/objclass/ObjectCopyQuestion.java
@@ -6,21 +6,20 @@ import java.util.HashMap;
  * 深拷贝
  * 浅拷贝
  */
-public class ObjectCopyQuestion implements Cloneable{
-
+public class ObjectCopyQuestion implements Cloneable {
 
 
     private int age = 0;
-    private HashMap<String,String> cache = new HashMap<>();
+    private HashMap<String, String> cache = new HashMap<>();
 
     @Override
     public boolean equals(Object obj) {
-        if (obj == null){
+        if (obj == null) {
             return false;
         }
-        if (obj instanceof ObjectCopyQuestion ){
+        if (obj instanceof ObjectCopyQuestion) {
             ObjectCopyQuestion var = (ObjectCopyQuestion) obj;
-            if (this.age == var.age){
+            if (this.age == var.age) {
                 return true;
             }
 
@@ -29,11 +28,23 @@ public class ObjectCopyQuestion implements Cloneable{
     }
 
 
-
-    public static void main(String[] args) throws CloneNotSupportedException{
+    public static void main(String[] args) throws CloneNotSupportedException {
         ObjectCopyQuestion obj = new ObjectCopyQuestion();
         Object clone = obj.clone();
         System.out.println(obj == clone);
         System.out.println(obj.equals(clone));
+        try {
+            add(10);
+        }catch (RuntimeException e){
+            System.out.println("runtime exception");
+        }catch (Exception e){
+            System.out.println("exception");
+        }
+    }
+
+    public static void add(int age) {
+        if (age == 10) {
+            throw new RuntimeException("已存在");
+        }
     }
 }
