e3a09268408b7b84671e0363d2c81b5bfc3be8ea
BlackHe <1124681011@qq.com>
2019-09-01 21:33:16 +0800

    test

---
+++ src/main/java/com/peony/system/CPUProcessorsQuestion.java
@@ -0,0 +1,7 @@
+package com.peony.system;
+
+public class CPUProcessorsQuestion {
+    public static void main(String[] args) {
+        int N_CPU = Runtime.getRuntime().availableProcessors();
+    }
+}
