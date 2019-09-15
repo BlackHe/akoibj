3bd652f38600ed4be46b71e0373d568e9f0759ca
BlackHe <1124681011@qq.com>
2019-09-15 16:52:56 +0800

    print graph

---
+++ src/main/java/com/peony/interview/printGraph01.java
@@ -0,0 +1,25 @@
+package com.peony.interview;
+import static com.peony.utils.PrintUtils.*;
+
+/**
+ * 打印如下图形
+ * #
+ * ##
+ * ###
+ * ####
+ * #####
+ * ######
+ * #######
+ * ######
+ * #####
+ * ####
+ * ###
+ * ##
+ * #
+ */
+public class printGraph01 {
+
+    public static void main(String[] args) {
+        print("1");
+    }
+}
