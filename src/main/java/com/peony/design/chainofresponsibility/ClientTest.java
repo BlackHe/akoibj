2b031818bc8b5b307a3fce2b024d5db400075488
BlackHe <11124681011@qq.com>
2019-12-24 23:19:07 +0800

    chain of responsibility design pattern demo

---
+++ src/main/java/com/peony/design/chainofresponsibility/ClientTest.java
@@ -0,0 +1,24 @@
+package com.peony.design.chainofresponsibility;
+
+
+public class ClientTest {
+    public static void main(String[] args) {
+        AbstractLogger logger = new ConsoleLogger();
+        logger.log(AbstractLogger.ERROR,"Error log");
+        logger.log(AbstractLogger.FILE,"File log");
+        logger.log(AbstractLogger.CONSOLE,"Console log");
+
+        System.out.println();
+        final AbstractLogger fileLogger = new FileLogger();
+        fileLogger.log(AbstractLogger.ERROR,"Error log");
+        fileLogger.log(AbstractLogger.FILE,"File log");
+        fileLogger.log(AbstractLogger.CONSOLE,"Console log");
+
+        System.out.println();
+        final AbstractLogger errorLogger = new ErrorLogger();
+        errorLogger.log(AbstractLogger.ERROR,"Error log");
+        errorLogger.log(AbstractLogger.FILE,"File log");
+        errorLogger.log(AbstractLogger.CONSOLE,"Console log");
+
+    }
+}
---
+++ src/main/java/com/peony/design/chainofresponsibility/ConsoleLogger.java
@@ -0,0 +1,18 @@
+package com.peony.design.chainofresponsibility;
+
+public class ConsoleLogger extends AbstractLogger {
+
+    public ConsoleLogger(){
+        super.level = AbstractLogger.CONSOLE;
+    }
+
+    @Override
+    public void write(String message) {
+        System.out.println(this.getClass().getName() + ": " + message);
+    }
+
+    @Override
+    public AbstractLogger nextLogger() {
+        return new FileLogger();
+    }
+}
