22e714916b4a1c9625ae9e154e2d122f2c93dca9
BlackHe <1124681011@qq.com>
2020-12-07 17:08:58 +0800

    thread pool

---
+++ src/main/java/com/peony/MainTest2.java
@@ -0,0 +1,30 @@
+package com.peony;
+
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.Year;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class MainTest2 {
+
+    private static Thread t1 = null;
+    private static Thread t2 = null;
+    private static Thread t3 = null;
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+
+
+    }
+
+
+
+}
---
+++ src/main/java/com/peony/number/IntegerTest.java
@@ -0,0 +1,14 @@
+package com.peony.number;
+
+public class IntegerTest {
+
+    public static void main(String[] args) {
+        Integer a = 100,b = 100;
+        System.out.println(a == b);
+
+        Integer c = 1000,d = 1000;
+        System.out.println(c == d);
+
+        assert 18 > 2;
+    }
+}
