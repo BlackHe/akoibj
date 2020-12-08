81ca56c060439102aad81df0039fc3500026d495
BlackHe <1124681011@qq.com>
2020-12-08 11:46:16 +0800

    Log

--- src/main/java/com/peony/log/Log.java
+++ src/main/java/com/peony/log/Log.java
@@ -1,4 +1,17 @@
 package com.peony.log;
 
+import java.time.LocalDate;
+
 public class Log {
+
+    public static void info(String info,String... strings){
+        if (strings == null || strings.length == 0){
+            System.out.println(info);
+        }
+        System.out.printf(info + "\n",strings);
+    }
+
+    public static void main(String[] args) {
+        Log.info("今年是【%s】年", LocalDate.now().getYear()+"");
+    }
 }
