ed846839e98f7ac2bac6711305c089088702f961
BlackHe <11124681011@qq.com>
2020-12-09 22:19:32 +0800

    CyclicBarrier测试

--- src/main/java/com/peony/concurrent/CyclicBarrierTest.java
+++ src/main/java/com/peony/concurrent/CyclicBarrierTest.java
@@ -1,5 +1,6 @@
 package com.peony.concurrent;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.TimeUnit;
 
@@ -9,7 +10,8 @@ public class CyclicBarrierTest {
 
     public static void main(String[] args) {
 
-        CyclicBarrier barrier = new CyclicBarrier(5);
+        CyclicBarrier barrier = new CyclicBarrier(2);
+        CountDownLatch countDownLatch = new CountDownLatch(2);
 
         new Thread(() -> {
             try {
@@ -18,6 +20,7 @@ public class CyclicBarrierTest {
                 barrier.await();
                 System.out.println("线程1准备完成，go on");
                 System.out.println("线程1结束任务");
+                countDownLatch.countDown();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -30,11 +33,50 @@ public class CyclicBarrierTest {
                 barrier.await();
                 System.out.println("线程2准备完成，go on");
                 System.out.println("线程2结束任务");
+                countDownLatch.countDown();
             } catch (Exception e) {
                 e.printStackTrace();
             }
             System.out.println(Thread.currentThread().getName());
         },"t2").start();
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        barrier.reset();
+        System.out.println("barrier reset...");
+
+
+        new Thread(() -> {
+            try {
+                System.out.println("线程3准备中。。。");
+                sleep(1);
+                barrier.await();
+                System.out.println("线程3准备完成，go on");
+                System.out.println("线程3结束任务");
+                countDownLatch.countDown();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        },"t3").start();
+
+        new Thread(() -> {
+            try {
+                System.out.println("线程4准备中。。。");
+                sleep(2);
+                barrier.await();
+                System.out.println("线程4准备完成，go on");
+                System.out.println("线程4结束任务");
+                countDownLatch.countDown();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName());
+        },"t4").start();
+
     }
 
     public static void sleep(long seconds){
