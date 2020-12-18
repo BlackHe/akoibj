c0e7a09469508b4d9b75a7e18aa43de502523221
BlackHe <1124681011@qq.com>
2020-12-18 14:40:05 +0800

    两个线程交替输出-by locksupport

---
+++ src/main/java/com/peony/thread/TruningPrintByLockSupport.java
@@ -0,0 +1,43 @@
+package com.peony.thread;
+
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * 两个线程交替输出
+ * 即线程1输出 ABCDEFG
+ * 线程2输出 123456
+ */
+public class TruningPrintByLockSupport {
+
+    private static final char[] charI = "123456".toCharArray();
+    private static final char[] charC = "ABCDEF".toCharArray();
+
+    private static Thread t1,t2;
+
+    public static void main(String[] args) {
+
+        t1 = new Thread(() ->{
+            for (char c : charI) {
+                System.out.print(c);
+                LockSupport.unpark(t2);
+                LockSupport.park();
+            }
+        });
+
+        t2 = new Thread(() ->{
+            for (char c : charC) {
+                LockSupport.park();
+                System.out.print(c);
+                LockSupport.unpark(t1);
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+
+
+}
