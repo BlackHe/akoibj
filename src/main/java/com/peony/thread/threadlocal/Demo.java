cf87da84b22f35207db90b7776891157d6c58161
BlackHe <1124681011@qq.com>
2020-12-18 14:33:54 +0800

    threadlocal

--- src/main/java/com/peony/thread/threadlocal/Demo.java
+++ src/main/java/com/peony/thread/threadlocal/Demo.java
@@ -6,7 +6,9 @@ public class Demo {
     private String name;
 
 
-    private static ThreadLocal<Demo> tl = new ThreadLocal<>();
+    private static ThreadLocal<Demo> localSession = new ThreadLocal<>();
+
+    private static ThreadLocal<Demo> connectCtx = new ThreadLocal<>();
 
     public Demo(String name) {
         this.name = name;
@@ -23,23 +25,23 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        tl.set(new Demo(Thread.currentThread().getName()));
+        localSession.set(new Demo(Thread.currentThread().getName()+"-的线程本地会话"));
 
-        tl.set(new Demo(Thread.currentThread().getName()));
+        connectCtx.set(new Demo(Thread.currentThread().getName()+"-的数据库连接"));
 
         t1 = new Thread(() -> {
-            tl.set(new Demo(Thread.currentThread().getName()));
+            localSession.set(new Demo(Thread.currentThread().getName()));
             try {
                 t2.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("t1: " + tl.get());
+            System.out.println("t1: " + localSession.get().name);
         }, "t1");
 
         t2 = new Thread(() -> {
-            tl.set(new Demo(Thread.currentThread().getName()));
-            System.out.println("t2: " + tl.get());
+            localSession.set(new Demo(Thread.currentThread().getName()+"-的线程本地会话"));
+            System.out.println("t2: " + localSession.get().name);
         }, "t2");
 
         t1.start();
@@ -50,6 +52,7 @@ public class Demo {
             e.printStackTrace();
         }
 
-        System.out.println("main: " + tl.get());
+        System.out.println("main: " + localSession.get().name);
+        System.out.println("main: " + connectCtx.get().name);
     }
 }
