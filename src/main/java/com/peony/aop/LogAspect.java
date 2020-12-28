b1471a3923a838b3404923b8804178dde0d4f41d
BlackHe <1124681011@qq.com>
2020-12-28 18:25:54 +0800

    AOP源码代码示例:ThreadLocal统计方法执行耗时

--- src/main/java/com/peony/aop/LogAspect.java
+++ src/main/java/com/peony/aop/LogAspect.java
@@ -14,6 +14,8 @@ public class LogAspect {
 
     private static final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
+    private static final ThreadLocal<Instant> LOCAL_RUNNING_INSTANT = new ThreadLocal<>();
+
     @Pointcut("execution(* com.peony.aop.*.*(..))")
     public void pointCut(){
 
@@ -21,6 +23,7 @@ public class LogAspect {
 
     @Before("pointCut()")
     public void before(JoinPoint joinPoint){
+        LOCAL_RUNNING_INSTANT.set(Instant.now());
         Object[] args = joinPoint.getArgs();
         StringBuilder sb = new StringBuilder();
         for (Object arg : args) {
@@ -38,14 +41,17 @@ public class LogAspect {
         System.out.println("["+Thread.currentThread().getName()+"]时间：["+ DTF.format(LocalDateTime.now()) +"],请求方法：["+joinPoint.getSignature().toShortString()+"],请求参数：["+sb.toString().substring(1)+"]");
     }
 
-    @After("pointCut()")
+//    @After("pointCut()")
     public void after(JoinPoint joinPoint){
-        System.out.println("后置 切面拦截");
+//        System.out.println("后置 切面拦截");
     }
 
     @AfterReturning(pointcut = "pointCut()",returning = "result")
     public void afterReturn(JoinPoint joinPoint,Object result){
-        System.out.println("["+Thread.currentThread().getName()+"]时间：["+ DTF.format(LocalDateTime.now()) +"],请求方法：["+joinPoint.getSignature().toShortString()+"],返回值：["+result+"]");
+        Instant begin = LOCAL_RUNNING_INSTANT.get();
+        LOCAL_RUNNING_INSTANT.remove();
+        Long cost = Instant.now().toEpochMilli() - begin.toEpochMilli();
+        System.out.println("["+Thread.currentThread().getName()+"]时间：["+ DTF.format(LocalDateTime.now()) +"],请求方法：["+joinPoint.getSignature().toShortString()+"],返回值：["+result+"],执行耗时：["+cost+"]毫秒");
     }
 
     @AfterThrowing("pointCut()")
