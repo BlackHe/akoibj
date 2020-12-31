aeee3977cf55f8ac2ef4f6f028021614722aaaee
BlackHe <1124681011@qq.com>
2020-12-31 18:05:15 +0800

    验证postProcessor的注入时机

--- src/main/java/com/peony/postprocess/MyService.java
+++ src/main/java/com/peony/postprocess/MyService.java
@@ -1,9 +1,16 @@
 package com.peony.postprocess;
 
 
+import org.springframework.transaction.annotation.Transactional;
+
 public class MyService {
 
     public MyService() {
         System.out.println("myService instanin......");
+    }
+
+    @Transactional
+    public void update(){
+
     }
 }
