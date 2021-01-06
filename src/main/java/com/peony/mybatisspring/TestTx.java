17501bd4f5976248e48b7e29de1b29572788b042
BlackHe <11124681011@qq.com>
2021-01-06 23:24:21 +0800

    sqlSession源码分析

--- src/main/java/com/peony/mybatisspring/TestTx.java
+++ src/main/java/com/peony/mybatisspring/TestTx.java
@@ -2,6 +2,7 @@ package com.peony.mybatisspring;
 
 import com.peony.ibatis.entity.BookDO;
 import com.peony.ibatis.entity.ReaderDO;
+import com.peony.ibatis.repository.BookDAO;
 import com.peony.ibatis.service.BookService;
 import com.peony.ibatis.service.ProcessService;
 import com.peony.mybatisspring.config.TransactionConfig;
@@ -12,8 +13,12 @@ public class TestTx {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TransactionConfig.class);
 
-        ProcessService processService = ac.getBean(ProcessService.class);
-
-        processService.addedBook(new BookDO("micro service"),new ReaderDO("james"));
+//        ProcessService processService = ac.getBean(ProcessService.class);
+//        processService.addedBook(new BookDO("micro service"),new ReaderDO("james"));
+//
+        BookDAO bookDAO = ac.getBean(BookDAO.class);
+        System.out.println(bookDAO.select(1));
+        System.out.println(bookDAO.select(2));
+        System.out.println(bookDAO.select(3));
     }
 }
