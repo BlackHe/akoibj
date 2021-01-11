720079191bd6df2279370c94a154746d9e7ce6d5
BlackHe <1124681011@qq.com>
2021-01-11 17:52:28 +0800

    spring 事件原理剖析之 自定义 SimpleApplicationEventMulticaster 实现异步执行监听器

--- src/main/java/com/peony/event/MainTest.java
+++ src/main/java/com/peony/event/MainTest.java
@@ -2,25 +2,54 @@ package com.peony.event;
 
 import com.peony.event.domain.event.ConnectEvent;
 import com.peony.event.domain.publisher.DomainEventPublisher;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import java.util.concurrent.*;
 
 @Configuration
 @ComponentScan
-public class MainTest {
+public class MainTest implements ApplicationContextAware {
 
 
+    private static ExecutorService executor = Executors.newFixedThreadPool(5);
+
+    private ApplicationContext context;
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(MainTest.class);
         DomainEventPublisher domainEventPublisher = context.getBean(DomainEventPublisher.class);
         new MainTest().publish(domainEventPublisher);
+        executor.shutdown();
     }
 
     public void publish(DomainEventPublisher domainEventPublisher){
-        domainEventPublisher.publishEvent(new ConnectEvent(this,"187.99.0.98"));
+        domainEventPublisher.publishEvent(new ConnectEvent(this,"187.99.0.08"));
+        domainEventPublisher.publishEvent(new ConnectEvent(this,"187.99.0.18"));
+        domainEventPublisher.publishEvent(new ConnectEvent(this,"187.99.0.28"));
+        domainEventPublisher.publishEvent(new ConnectEvent(this,"187.99.0.38"));
+        domainEventPublisher.publishEvent(new ConnectEvent(this,"187.99.0.48"));
+        domainEventPublisher.publishEvent(new ConnectEvent(this,"187.99.0.58"));
+    }
+
+    @Bean(name = AbstractApplicationContext.APPLICATION_EVENT_MULTICASTER_BEAN_NAME)
+    public SimpleApplicationEventMulticaster applicationEventMulticaster(){
+        SimpleApplicationEventMulticaster simpleApplicationEventMulticaster = new SimpleApplicationEventMulticaster(context);
+        simpleApplicationEventMulticaster.setTaskExecutor(executor);
+        return simpleApplicationEventMulticaster;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
     }
 }
--- src/main/java/com/peony/event/domain/listen/ConnectListener.java
+++ src/main/java/com/peony/event/domain/listen/ConnectListener.java
@@ -2,14 +2,16 @@ package com.peony.event.domain.listen;
 
 import com.peony.event.domain.event.ConnectEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConnectListener {
 
     @EventListener(ConnectEvent.class)
+    @Async
     public String onConnect(ConnectEvent event){
-        System.out.println("lisened connect event: "+event.getClient());
+        System.out.println(Thread.currentThread().getName()+" lisened connect event: "+event.getClient());
         return "process ok";
     }
 }
