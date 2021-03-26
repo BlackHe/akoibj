7ca7df741d3aa8c0d608087da6b871b76983ecb2
BlackHe <1124681011@qq.com>
2021-03-26 17:43:30 +0800

    线程中断实战

---
+++ src/main/java/com/peony/collection/ArrayListQuestion.java
@@ -0,0 +1,18 @@
+package com.peony.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListQuestion {
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>(4);
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+
+        list.add("3");
+        list.add("3");
+        list.add("3");
+    }
+}
---
+++ src/main/java/com/peony/thread/interrupt/TestThread.java
@@ -0,0 +1,50 @@
+package com.peony.thread.interrupt;
+
+import com.peony.log.Log;
+import com.peony.utils.Sleeper;
+
+public class TestThread {
+
+
+
+    public static void main(String[] args) {
+
+        Thread thread01 = new Thread(new TaskA(),"THREAD-01");
+        thread01.start();
+        Sleeper.sleep(2);
+        Log.info("主线程将THREAD-01 中断");
+        thread01.interrupt();
+        for (int i = 0; i < 100; i++) {
+            Sleeper.sleep(1);
+            Log.info("主线程还在执行");
+        }
+
+
+    }
+
+    static class TaskA implements Runnable{
+        @Override
+        public void run() {
+            for (int i = 0; i < 100; i++) {
+                Log.info("task execute: "+ i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Log.info("线程【%s】出现中断异常...\n"+Thread.currentThread().getName());
+                    Log.info(e.getMessage());
+                    Log.info("%s线程相应中断，终止执行",Thread.currentThread().getName());
+                    break;
+                }
+            }
+        }
+    }
+
+    class TaskB implements Runnable{
+        @Override
+        public void run() {
+            for (int i = 0; i < 100; i++) {
+                System.out.println("taskB execute: "+ i);
+            }
+        }
+    }
+}
